06d5dd9aeda194774d9a007c36e4b83867f3d443
github username <github email address>
2018-06-04 20:48:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+    get '/' do
+      "Hello World"
+    end
+
+    get '/newteam' do
+      erb :newteam
+    end
+
+    post '/team' do
+
+      # @position_array = [] ###Not formmated properly
+      @formatted_position_array = ["Team Name:", "Coach:", "Point Guard:", "Shooting Guard:", "Power Forward:", "Small Forward:", "Center:"]
+      @player_array = []
+
+      params.each do |position, player|
+        #@position_array << position won't be formatted properly
+        @player_array << player
+      end
+
+      erb :team
+    end
 
 end
